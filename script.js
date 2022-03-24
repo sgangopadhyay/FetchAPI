46ce976041dad6a5fd4354e231067809cdee2dcb
Suman Gangopadhyay <linuxgurusuman@gmail.com>
2022-03-24 13:10:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,31 @@ guvi
 fetch(url)
   .then((response) => response.json())
   .then((data) => console.log(JSON.stringify(data)));
+
+// POST Data / INSERT Data into an API Server in JSON format
+
+const img_link =
+  "https://images.unsplash.com/photo-1625398407796-82650a8c135f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80";
+
+// data to be inserted
+const data = {
+  first_name: "Suman",
+  last_name: "Gangopadhyay",
+  location: "Bangalore",
+  image: img_link,
+};
+
+fetch(url, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(data),
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("success:", data);
+  })
+  .catch((error) => {
+    console.log("error: ", error);
+  });
